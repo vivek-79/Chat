67e85d4d89eecdbfa0a6cad29647eb03d698f5c9
vivek-79 <vivekroy5667@gmail.com>
2024-11-29 09:35:03 +0530

    group-bug

--- src/app/(root)/chats/group-info/page.jsx
+++ src/app/(root)/chats/group-info/page.jsx
@@ -1,50 +1,123 @@
-import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { compare } from "bcryptjs";
+"use client";
 
-import { connectToDB } from "../../../../mongodb";
-import User from "../../../../models/user";
+import Loader from "@components/Loader";
+import { GroupOutlined, PersonOutline } from "@mui/icons-material";
+import { CldUploadButton } from "next-cloudinary";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 
-const handler = NextAuth({
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      async authorize(credentials, req) {
-        if (!credentials.email || !credentials.password) {
-          throw new Error("Invalid email or password");
-        }
+const GroupInfo = () => {
+  const [loading, setLoading] = useState(true);
+  const [chat, setChat] = useState({});
 
-        await connectToDB()
+  const { chatId } = useParams();
 
-        const user = await User.findOne({ email: credentials.email });
-
-        if (!user || !user?.password) {
-          throw new Error("Invalid email or password");
-        }
-
-        const isMatch = await compare(credentials.password, user.password);
-
-        if (!isMatch) {
-          throw new Error("Invalid password");
-        }
-
-        return user
-      },
-    }),
-  ],
-
-  secret: process.env.NEXTAUTH_SECRET,
-
-  callbacks: {
-    async session({session}) {
-      const mongodbUser = await User.findOne({ email: session.user.email })
-      session.user.id = mongodbUser._id.toString()
-
-      session.user = {...session.user, ...mongodbUser._doc}
-
-      return session
+  const getChatDetails = async () => {
+    try {
+      const res = await fetch(`/api/chats/${chatId}`);
+      const data = await res.json();
+      setChat(data);
+      setLoading(false);
+      reset({
+        name: data?.name,
+        groupPhoto: data?.groupPhoto,
+      });
+    } catch (error) {
+      console.log(error);
     }
-  }
-});
+  };
 
-export { handler as GET, handler as POST };
+  useEffect(() => {
+    if (chatId) {
+      getChatDetails();
+    }
+  }, [chatId]);
+
+  const {
+    register,
+    watch,
+    setValue,
+    reset,
+    handleSubmit,
+    formState: { error },
+  } = useForm();
+
+  const uploadPhoto = (result) => {
+    setValue("groupPhoto", result?.info?.secure_url);
+  };
+
+  const router = useRouter();
+
+  const updateGroupChat = async (data) => {
+    setLoading(true);
+    try {
+      const res = await fetch(`/api/chats/${chatId}/update`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      setLoading(false);
+
+      if (res.ok) {
+        router.push(`/chats/${chatId}`);
+      }
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return loading ? (
+    <Loader />
+  ) : (
+    <div className="profile-page">
+      <h1 className="text-heading3-bold">Edit Group Info</h1>
+
+      <form className="edit-profile" onSubmit={handleSubmit(updateGroupChat)}>
+        <div className="input">
+          <input
+            {...register("name", {
+              required: "Group chat name is required",
+            })}
+            type="text"
+            placeholder="Group chat name"
+            className="input-field"
+          />
+          <GroupOutlined sx={{ color: "#737373" }} />
+        </div>
+        {error?.name && <p className="text-red-500">{error.name.message}</p>}
+
+        <div className="flex items-center justify-between">
+          <img
+            src={watch("groupPhoto") || "/assets/group.png"}
+            alt="profile"
+            className="w-40 h-40 rounded-full"
+          />
+          <CldUploadButton
+            options={{ maxFiles: 1 }}
+            onSuccess={uploadPhoto}
+            uploadPreset="upecg01j"
+          >
+            <p className="text-body-bold">Upload new photo</p>
+          </CldUploadButton>
+        </div>
+
+        <div className="flex flex-wrap gap-3">
+          {chat?.members?.map((member, index) => (
+            <p className="selected-contact" key={index}>{member.username}</p>
+          ))}
+        </div>
+
+        <button className="btn" type="submit">
+          Save Changes
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default GroupInfo;
